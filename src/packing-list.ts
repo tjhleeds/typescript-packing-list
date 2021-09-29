8fc4b34d40a4d17927e2293ea5b3f1966fa351bb
tjhleeds <hilton.tim@gmail.com>
2021-09-29 14:55:19 +0100

    Fixed incorrect `ul` tags - replaced with `li` on packing list & settings pages

--- src/packing-list.ts
+++ src/packing-list.ts
@@ -2,8 +2,8 @@ export class PackingListPage {
   constructor() {
     const appDiv: HTMLElement = document.getElementById('packing-list');
     const numberOfItems = 5;
-    const uls = new Array[numberOfItems].keys().map(x => `<ul>Item ${x}</ul>`);
-    const html = uls.reduce((str, all) => all + str, '');
+    const listItems = new Array[numberOfItems].keys().map(x => `<li>Item ${x}</li>`);
+    const html = listItems.reduce((str, all) => all + str, '');
     appDiv.innerHTML = html;
   }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -2,9 +2,9 @@ export class SettingsPage {
   constructor(){
     const appDiv: HTMLElement = document.getElementById('settings-list');
     const numberOfSettings = 5;
-    const uls = new Array[numberOfSettings].keys()
-      .map(x => `<ul>Setting ${x}</ul>`);
-    const html = uls.reduce((str, all) => all + str, '');
+    const listItems = new Array[numberOfSettings].keys()
+      .map(x => `<li>Setting ${x}</li>`);
+    const html = listItems.reduce((str, all) => all + str, '');
     appDiv.innerHTML = html;
   }
 }
